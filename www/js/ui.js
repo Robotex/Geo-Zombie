178b5f14a8c994d45ed1b892d89378cdb00ef1d5
Robotex <robotex@altervista.org>
2015-03-15 17:02:50 +0100

    css calc alternative

--- www/js/ui.js
+++ www/js/ui.js
@@ -2,8 +2,12 @@ $(document).ready(function() {
     $( window ).resize(function() {
         var viewportHeight = $( window ).height();
         var viewportWidth = $( window ).width();
-        var controlWidth = (viewportHeight/5);
-        $('#map-container').css('width', viewportWidth-controlWidth);
+        var controlWidth = viewportHeight/5;
+        var containerWidth = viewportWidth-controlWidth
+        var controlHeight = controlWidth;
+        $('#map-container').css('width', containerWidth);
+        $('#controls').css('width', controlWidth);
+        $('.control-button').css('height', controlHeight);
     });
     
     $(window).resize();
